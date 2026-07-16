a4370d11048cf65f6f248eba724cbcd2b9b4e30a
omolewu sunday <omolewu12@gmail.com>
2026-07-16 13:28:40 +0100

    feat: implement ticket price calculator

---
+++ src/pairtest/TicketPriceCalculator.js
@@ -0,0 +1,17 @@
+export default class TicketPriceCalculator {
+    static #TICKET_PRICES = {
+        ADULT: 25,
+        CHILD: 15,
+        INFANT: 0
+    };
+
+    calculateTotalPrice(ticketTypeRequests) {
+        return ticketTypeRequests.reduce((total, request) => {
+            const ticketType = request.getTicketType();
+            const numberOfTickets = request.getNoOfTickets();
+            const price = TicketPriceCalculator.#TICKET_PRICES[ticketType];
+            return total + (price * numberOfTickets);
+        }, 0);
+    }
+
+}
