af722989931fa2d679159339767542735c62f6b7
omolewu sunday <omolewu12@gmail.com>
2026-07-16 14:56:51 +0100

    feat: implement seat reservation calculator

---
+++ src/pairtest/SeatReservationCalculator.js
@@ -0,0 +1,17 @@
+
+export default class SeatReservationCalculator {
+    static #SEAT_ALLOCATION = {
+        ADULT: 1,
+        CHILD: 1,
+        INFANT: 0
+    };
+    calculateTotalSeats(ticketTypeRequests) {
+        return ticketTypeRequests.reduce((totalSeats, request) => {
+            const ticketType = request.getTicketType();
+            const numberOfTickets = request.getNoOfTickets();
+            const seats = SeatReservationCalculator.#SEAT_ALLOCATION[ticketType];
+
+            return totalSeats + (seats * numberOfTickets);
+        }, 0);
+    }
+}
